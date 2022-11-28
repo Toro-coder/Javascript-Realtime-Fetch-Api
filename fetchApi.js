56e76bd6c6835f901f81527b1bc92593a6398ca3
CYNTHIA TOROITICH <cynthiajepkemei11@gmail.com>
2022-11-28 16:25:12 +0300

    removed redundant lines of code

--- fetchApi.js
+++ fetchApi.js
@@ -5,18 +5,15 @@ function startLiveUpdate(route) {
         data: "y",
         success: function (response) {
         var data = JSON.stringify(response);
+        data = data.substring(10); // removes the first 19 characters
+            data = data.substring(0, data.length - 2); // removes the last 3 characters
+            data = data.split(",");
         // stringify total and weekly users
         if(route === "fetchUsers"){
-           data = data.substring(10); // removes the first 19 characters
-           data = data.substring(0, data.length - 2); // removes the last 3 characters
-           data = data.split(",");
            document.getElementById("counts").innerHTML += data + "<br><br>";
         }
         // stringify county users
         else if(route === "countyusers"){
-           data = data.substring(10); // removes the first 19 characters
-           data = data.substring(0, data.length - 2); // removes the last 3 characters
-           data = data.split(",");
            for(var i=0; i <= 93; i++) {
                if(i % 2) {
                    document.getElementById("counts").innerHTML += data[i].substring(0, data[i].length - 1) + "<br><br>";
@@ -29,9 +26,6 @@ function startLiveUpdate(route) {
         }
         // stringify gender
         else if (route === "gender"){
-            data = data.substring(10); // removes the first 19 characters
-            data = data.substring(0, data.length - 2); // removes the last 3 characters
-            data = data.split(",");
             for(var i=0; i <= 6; i++) {
                if(i % 2) {
                    document.getElementById("counts").innerHTML += data[i].substring(0, data[i].length - 1) + "<br><br>";
@@ -44,9 +38,7 @@ function startLiveUpdate(route) {
         }
        // stringify gender
        else if (route === "age_range"){
-            data = data.substring(10); // removes the first 19 characters
-            data = data.substring(0, data.length - 2); // removes the last 3 characters
-            data = data.split(",");
+
             for(var i=0; i <= 14; i++) {
                if(i % 2) {
                    document.getElementById("counts").innerHTML += data[i].substring(0, data[i].length - 1) + "<br><br>";
